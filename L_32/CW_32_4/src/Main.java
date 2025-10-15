d55b024e4e01a3d7289aaa0e0b32c6ea31e2219c
vkadi-budetak <126949497+vkadi-budetak@users.noreply.github.com>
2025-10-15 21:53:41 +0200

    HW 32

--- L_32/CW_32_4/src/Main.java
+++ L_32/CW_32_4/src/Main.java
@@ -1,7 +1,39 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
+        List<MailDeliveryService> services = new ArrayList<>(Arrays.asList(new Pigeon(), new DHL(), new Email()));
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("=== Вас приветсвует Delivery Service ===");
+        System.out.println("Вы можете отправить свою посылку:\n " +
+                "Pigeon -> Введите 1\n DHL -> Введите 2\n Email -> Введите 3\n Для выхода -> Введите 0");
+
+        while (true) {
+            System.out.print("Ваш выбор: ");
+            String choice = sc.nextLine().trim();
+            if (choice.equals("0")) {
+                System.out.println("Выход.");
+                break;
+            } else if (choice.equals("1")) {
+                services.get(0).sendMail();     // Pigeon
+                break;
+            } else if (choice.equals("2")) {
+                services.get(1).sendMail();     // DHL
+                break;
+            } else if (choice.equals("3")) {
+                services.get(2).sendMail();     // Email
+                break;
+            } else {
+                System.out.println("Некорректный ввод.\n" +
+                        " Pigeon -> Введите 1\n DHL -> Введите 2\n Email -> Введите 3\n Для выхода -> Введите 0");
+            }
+        }
 
 
     }
