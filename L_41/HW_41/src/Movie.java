55a217c477475b1f4c96037f637d4347debc73e9
vkadi-budetak <126949497+vkadi-budetak@users.noreply.github.com>
2025-10-28 19:58:00 +0100

    HW 41

---
+++ L_41/HW_41/src/Movie.java
@@ -0,0 +1,42 @@
+import java.util.Objects;
+
+public class Movie {
+    public String title;
+    public int year;
+    public double rating;
+
+    public Movie(String title, int year, double rating) {
+        this.title = title;
+        this.year = year;
+        this.rating = rating;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+    public int getYear() {
+        return year;
+    }
+    public double getRating() {
+        return rating;
+    }
+
+    @Override
+    public String toString() {
+        return "title: " + title +
+                ", year: " + year +
+                ", rating: " + rating;
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (!(o instanceof Movie movie)) return false;
+
+        return year == movie.year && Objects.equals(title, movie.title);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(title);
+    }
+}
---
+++ L_41/HW_41/src/StreamAppl.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class StreamAppl {
+    public static void main(String[] args) {
+        Movie[] movies = {
+                new Movie("a", 2022, 4.0),
+                new Movie("b", 2021, 4.5),
+                new Movie("c", 2022, 4.5),
+                new Movie("d", 2021, 3.5),
+                new Movie("e", 2023, 5.0),
+                new Movie("e", 2021, 5.0)
+        };
+
+        System.out.println(Arrays.toString(movies));
+
+        // 1. вывод уникальный рейтинги фильмов
+        Stream<Movie> stream = Arrays.stream(movies);
+        stream.map(movie -> movie.rating).distinct().forEach(System.out::println);
+        System.out.println("================================");
+
+        // 2. выводим первые 2 уникальные года фильмов
+        Arrays.stream(movies)
+                .map(movie -> movie.year).distinct().limit(2).forEach(System.out::println);
+        System.out.println("================================");
+
+        // 3. выводим только второй уникальный год из всех фильмов
+        Arrays.stream(movies)
+                .map(movie -> movie.year).distinct().skip(1).limit(1).forEach(System.out::println);
+        System.out.println("================================");
+
+        // 4. выводим на консоль только предпоследний элемент вашего массива.
+        Arrays.stream(movies)
+                .skip(movies.length -2).limit(1).forEach(System.out::println);
+
+    }
+}
+
+/*
+Для класса Movie из классной работы решите следующие задачи
+1. Создайте стрим, который даст вам и выведет на консоль
+уникальные рейтинги фильмов.
+2. Создайте стрим, который даст вам и выведет на консоль
+только 2 первых уникальных года фильмов.
+3. Создайте стрим, который даст вам и выведет на консоль
+только второй уникальный год из всех фильмов.
+4. Создайте стрим, который выведет на консоль только
+предпоследний элемент вашего массива.
+ */
